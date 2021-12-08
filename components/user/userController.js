d10c1c3114587a3798b25c2c2ece80c989c1709e
thientoan0101 <thientoancubi@gmail.com>
2021-12-08 22:10:43 +0700

    add avartar, save update profile

--- components/user/userController.js
+++ components/user/userController.js
@@ -2,8 +2,9 @@ const userService = require('./userService');
 
 const cloudinary = require('cloudinary').v2;
 const formidable = require('formidable');
-
+const {check, validationResult} = require('express-validator');
 const User = require('../../models/userModel');
+
 cloudinary.config({ 
     cloud_name: process.env.CLOUD_NAME, 
     api_key: process.env.API_KEY, 
@@ -30,12 +31,13 @@ exports.updateImage = async (req, res) => {
         if (user) {
             console.log("file: \n\n" + JSON.stringify(files));
             console.log("field: \n\n" + JSON.stringify(fields));
-            cloudinary.uploader.upload(files.image.filepath, { public_id: `mern/users/${user._id}/${user.username}`,overwrite: true, width: 192, height: 192, crop: "scale"})
-        
-            //const newLink = "https://res.cloudinary.com/dgci6plhk/image/upload/images/users/{{this._id}}/{{this.nameImage}}.jpg"
-            const newLink = "https://res.cloudinary.com/mernteam/image/upload/v1638468308/mern/users/{{user._id}}/{{user.nameImage}}.jpg"
+            cloudinary.uploader.upload(files.image.filepath, { public_id: `user/${user._id}/${user.username}`,overwrite: true, width: 192, height: 192, crop: "scale", fetch_format: "jpg"})
+            
+            const newLink = "https://res.cloudinary.com/dgci6plhk/image/upload/v1638968024/user/" + user._id + "/" + user.username + ".jpg";
+            //const newLink = "https://res.cloudinary.com/mernteam/image/upload/v1638468308/mern/users/" + user._id + "/" + user.nameImage + ".jpg"
             userService.updateImage(newLink, id);
-            res.redirect('/');
+            
+            res.redirect('/users/profile');
         }
         
         // // res.redirect('/users/profile');
@@ -48,3 +50,43 @@ exports.updateImage = async (req, res) => {
 
     })
 };
+
+
+exports.saveUpdate = async (req, res) => {
+    const id = req.params.id;  
+    
+    
+    const name = await req.body.name;
+    const email = await req.body.email;
+    const phone = await req.body.phone;
+    const username = await req.body.username;
+    const password = await req.body.password;
+    const password2 = await req.body.password2;
+    const address = await req.body.address;
+
+    
+    check('name', 'Name is required!').notEmpty();
+    check('email', 'Email is required!').isEmail();
+    check('phone', 'Email is required!').notEmpty();
+    check('address', 'Email is required!').notEmpty();
+    check('username', 'Username is required!').notEmpty();
+    check('password', 'Password is required!').notEmpty();
+    check('password2', 'Passwords do not match!').equals(password);
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        console.log("loi empty validation");
+        res.redirect("/users/profile");
+    }
+    else {
+       try {
+            await userService.updateUser(id, name, email, phone, address, username, password);
+            //console.log("body: \n" + JSON.stringify(req.body));
+            // res.locals.messages = "Update successfull"
+            res.redirect('/users/profile');
+       } catch (Exception) {
+            res.redirect('/users/profile');
+       }
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const userController = require("../components/user/userController")
-
+const authGuard = require('../middlewares/authGuard');
 /* GET users listing. */
 
 
 
-router.get('/profile', userController.getProfile);
+router.get('/profile', authGuard.hasLogin, userController.getProfile);
 
 
-router.post('/update_avt/:id', userController.updateImage);
+router.post('/update_avt/:id', authGuard.hasLogin, userController.updateImage);
+
+router.post('/save/:id', authGuard.hasLogin, userController.saveUpdate);
+
 
 module.exports = router;
